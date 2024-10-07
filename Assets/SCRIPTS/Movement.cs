789a7941a9a80ff4ff8c76ecdca7b8333506aaea
daniiela08 <danielabarrosoruiz@gmail.com>
2024-10-07 20:36:07 +0200

    .

--- Assets/SCRIPTS/Movement.cs
+++ Assets/SCRIPTS/Movement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float fuerzasalto;
     private Rigidbody rb;
     public float fuerza;
+    Vector3 salto; 
     void Start()
     {
         rb = GetComponent < Rigidbody >();
@@ -15,10 +17,17 @@ public class Movement : MonoBehaviour
     {
         float movX = Input.GetAxis("Horizontal");
         float movZ = Input.GetAxis("Vertical");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            salto = new Vector3(0, 1, 0);
+            rb.AddForce(salto*fuerzasalto, ForceMode.Impulse);
+        }
+      
 
         Vector3 movimiento = new Vector3(movX, 0, movZ);
 
         rb.AddForce(movimiento * fuerza);
+        //rb.velocity += new Vector3(0, -9.81f * 4, 0);
     }
     void Update()
     {
