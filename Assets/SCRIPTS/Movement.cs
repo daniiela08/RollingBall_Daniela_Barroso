34557e6004f01537a9794b7079d3bf01fc6a47a8
daniiela08 <danielabarrosoruiz@gmail.com>
2024-10-09 17:26:52 +0200

    avancesss

--- Assets/SCRIPTS/Movement.cs
+++ Assets/SCRIPTS/Movement.cs
@@ -7,30 +7,33 @@ public class Movement : MonoBehaviour
     [SerializeField] float fuerzasalto;
     private Rigidbody rb;
     public float fuerza;
-    Vector3 salto; 
+    Vector3 salto;
+    Vector3 movimiento;
     void Start()
     {
         rb = GetComponent < Rigidbody >();
     }
 
-    void FixedUpdate()
+    
+    void Update()
     {
         float movX = Input.GetAxis("Horizontal");
         float movZ = Input.GetAxis("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             salto = new Vector3(0, 1, 0);
-            rb.AddForce(salto*fuerzasalto, ForceMode.Impulse);
+            rb.AddForce(salto * fuerzasalto, ForceMode.Impulse);
         }
-      
+         movimiento = new Vector3(movX, 0, movZ);
+    }
+    void FixedUpdate()
+    {
 
-        Vector3 movimiento = new Vector3(movX, 0, movZ);
+
+
+
 
         rb.AddForce(movimiento * fuerza);
         //rb.velocity += new Vector3(0, -9.81f * 4, 0);
-    }
-    void Update()
-    {
-        
     }
 }
